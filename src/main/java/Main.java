ab688ad12ccd1c70038c3e12c79783324d667537
benbi-hao <fahaing@163.com>
2022-07-14 21:52:06 +0800

    update TreeRecur

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,11 @@
+import leetcode.ds.ListNode;
 import leetcode.Solution;
 
 public class Main {
     public static void main(String[] args){
-        System.out.println("This is main function.");
+        
+        ListNode head = new ListNode(1, new ListNode(2, new ListNode(2, new ListNode(1))));
+
+        System.out.println(new Solution().isPalindrome(head));
     }
 }
--- src/main/java/leetcode/Solution.java
+++ src/main/java/leetcode/Solution.java
@@ -1,6 +1,8 @@
 package leetcode;
 
 import leetcode.ds.ListNode;
+import leetcode.ds.TreeNode;
+import java.lang.Math;
 
 public class Solution {
     /**
@@ -183,5 +185,130 @@ public class Solution {
         return curr;
     }
 
+    // 234. 回文链表
+    // 要求O(1)空间复杂度 
+    public boolean isPalindrome(ListNode head) {
+        if (head == null || head.next == null) return true;
+        ListNode slow = head, fast = head.next;
+        while (fast.next != null && fast.next.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+
+        ListNode half = slow.next;
+        if (fast.next == null) slow.next = null;
+        ListNode tail = reverseList(half);
+
+        while (head != null) {
+            if (head.val != tail.val) return false;
+            head = head.next;
+            tail = tail.next;
+        }
+
+        return true;
+    }
+
+    // 725. 分隔链表
+    public ListNode[] splitListToParts(ListNode head, int k) {
+        int len = 0;
+        for (ListNode p = head; p != null; p = p.next) { len++; }
+        int partLen = len / k;
+        int mod = len % k;
+        ListNode[] list = new ListNode[k];
+
+        for (int i = 0; i < k; i++) {
+            list[i] = head;
+            if (i < mod) {
+                head = cutList(head, partLen + 1);
+            }else{
+                head = cutList(head, partLen);
+            }
+        }
+
+        return list;
+    }
+
+    public ListNode cutList(ListNode head, int len) {
+        if (len == 0) return null;
+        ListNode p = head;
+        while (len > 1) {
+            p = p.next;
+            len--;
+        }
+        head = p.next;
+        p.next = null;
+        return head;
+    }
+
+    // 328. 奇偶链表
+    public ListNode oddEvenList(ListNode head) {
+        ListNode oddSentinel = new ListNode(0);
+        ListNode evenSentinel = new ListNode(0);
+        ListNode op = oddSentinel, ep = evenSentinel;
+
+        ListNode curr = head, next = null;
+        while(curr != null && curr.next != null) {
+            next = curr.next.next;
+            ep.next = curr.next;
+            ep = ep.next;
+            op.next = curr;
+            op = op.next;
+            curr = next;
+        }
+        
+        op.next = null;
+        ep.next = null;
+        if (curr != null) {
+            op.next = curr;
+            op = op.next;
+        }
+
+        op.next = evenSentinel.next;
+        return oddSentinel.next;
+    }
+
+
+
+    /**
+     * 树
+     */
     
+    // - 递归
+    // 104. 二叉树的最大深度
+    public int maxDepth(TreeNode root) {
+        if (root == null) return 0;
+        return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+    }
+
+    // 110. 平衡二叉树
+    public boolean isBalanced(TreeNode root) {
+        return isBalancedRecur(root) == -1 ? false : true;
+    }
+
+    public int isBalancedRecur(TreeNode root) {
+        if (root == null) return 0;
+        int leftDepth = isBalancedRecur(root.left);
+        if (leftDepth < 0) return -1;
+        int rightDepth = isBalancedRecur(root.right);
+        if (rightDepth < 0) return -1;
+        int diff = leftDepth - rightDepth;
+        if (diff < -1 || diff > 1) return -1;
+        if (diff > 0) return leftDepth + 1;
+        else return rightDepth + 1;
+    }
+
+    // 543. 二叉树的直径
+    private int maxDiameter = 0;
+    public int diameterOfBinaryTree(TreeNode root) {
+        diameterOfTreeAcross(root);
+        return maxDiameter;
+    }
+
+    public int diameterOfTreeAcross(TreeNode root) {
+        if (root == null) return 0;
+        int leftDepth = diameterOfTreeAcross(root.left);
+        int rightDepth = diameterOfTreeAcross(root.right);
+        this.maxDiameter = Math.max(leftDepth + rightDepth, this.maxDiameter);
+        return Math.max(leftDepth, rightDepth) + 1;
+    }
 }

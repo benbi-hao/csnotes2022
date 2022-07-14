e0cfddbaabb52d0a154a15b1963d0752bad97981
unknown <benbi-hao>
2022-07-15 00:03:21 +0800

    update treeRecur

--- src/main/java/leetcode/Solution.java
+++ src/main/java/leetcode/Solution.java
@@ -311,4 +311,25 @@ public class Solution {
         this.maxDiameter = Math.max(leftDepth + rightDepth, this.maxDiameter);
         return Math.max(leftDepth, rightDepth) + 1;
     }
+
+    // 226. 翻转树
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) return root;
+        TreeNode left = root.left;
+        root.left = invertTree(root.right);
+        root.right = invertTree(left);
+        return root;
+    }
+
+    // 617. 合并二叉树
+    public TreeNode mergeTrees(TreeNode root1, TreeNode root2) {
+        if (root1 == null) return root2;
+        if (root2 == null) return root1;
+        root1.left = mergeTrees(root1.left, root2.left);
+        root1.right = mergeTrees(root1.right, root2.right);
+        root1.val += root2.val;
+        return root1;
+    }
+
+    
 }

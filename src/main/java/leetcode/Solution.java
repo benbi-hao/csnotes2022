9af0eb5624ca2660a7ec362f83c96deb4f8f819a
benbi-hao <fahaing@163.com>
2023-06-20 23:34:58 +0800

    update binary search

--- src/main/java/leetcode/Solution.java
+++ src/main/java/leetcode/Solution.java
@@ -2115,6 +2115,23 @@ public class Solution {
         return nums[hi];
     }
 
+    // 278. 第一个错误的版本
+    public int firstBadVersion(int n) {
+        int lo = 1, hi = n;
+        while (lo < hi) {
+            int mid = lo + (hi - lo) / 2;
+            if (isBadVersion(mid)) {
+                hi = mid;
+            } else {
+                lo = mid + 1;
+            }
+        }
+        return hi;
+    }
+
+    private boolean isBadVersion(int version) {
+        return false;
+    }
     
 }
 

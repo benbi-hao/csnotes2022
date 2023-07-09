eb845e389a5ce63aa078cffaa98ecdc47c3ab126
benbi-hao <fahaing@163.com>
2023-07-09 19:02:25 +0800

    update dp

--- src/main/java/leetcode/Solution.java
+++ src/main/java/leetcode/Solution.java
@@ -2313,7 +2313,22 @@ public class Solution {
 
     // 413. 等差数列划分
     public int numberOfArithmeticSlices(int[] nums) {
-        
+        if (nums.length <= 2) return 0;
+        int[] dp = new int[nums.length];
+        dp[0] = 0;
+        dp[1] = 0;
+        for (int i = 2; i < dp.length; i++) {
+            if (nums[i] - nums[i - 1] == nums[i - 1] - nums[i - 2]) {
+                dp[i] = dp[i - 1] + 1;
+            } else {
+                dp[i] = 0;
+            }
+        }
+        int ret = 0;
+        for (int i : dp) {
+            ret += i;
+        }
+        return ret;
     }
 
     

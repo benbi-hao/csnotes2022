725202baf9a718714ab1726a25eb308a3acdee05
benbi-hao <fahaing@163.com>
2023-07-11 22:46:26 +0800

    update dp

--- src/main/java/leetcode/Solution.java
+++ src/main/java/leetcode/Solution.java
@@ -2331,7 +2331,32 @@ public class Solution {
         return ret;
     }
 
+    // 分割整数
+    // 343. 整数拆分
+    public int integerBreak(int n) {
+        int dp[] = new int[n + 1];
+        for (int i = 2; i <= n; i++) {
+            dp[i - 1] = Math.max(dp[i - 1], i - 1);
+            for (int j = 1; j < i; j++) {
+                dp[i] = Math.max(dp[i], dp[j] * (i - j));
+                // dp[i] = Math.max(dp[i], Math.max(dp[j] * (i - j), j * (i - j)))
+            }
+            
+        }
+        return dp[n];
+    }
     
+    // 279. 完全平方数
+    public int numSquares(int n) {
+        int dp[] = new int[n + 1];
+        for (int i = 1; i < dp.length; i++) {
+            dp[i] = i;
+            for (int j = 1; i - j * j >= 0; j++) {
+                dp[i] = Math.min(dp[i], dp[i - j * j] + 1);
+            }
+        }
+        return dp[n];
+    }
 }
 
 

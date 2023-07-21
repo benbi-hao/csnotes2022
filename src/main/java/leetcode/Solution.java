c3945fe698a41def2013b7a4ff64145e72aed4c1
benbi-hao <fahaing@163.com>
2023-07-21 21:28:28 +0800

    update dp

--- src/main/java/leetcode/Solution.java
+++ src/main/java/leetcode/Solution.java
@@ -2357,6 +2357,32 @@ public class Solution {
         }
         return dp[n];
     }
+
+    // 91. 解码方法
+    public int numDecodings(String s) {
+        if (s.charAt(0) == '0') return 0;
+        int n = s.length();
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+        dp[1] = 1;
+        char prev = s.charAt(0), curr;
+        for (int i = 2; i < n + 1; i++) {
+            curr = s.charAt(i - 1);
+            if (curr != '0') {
+                dp[i] += dp[i - 1];
+            }
+            if ((prev == '1' || prev == '2') && (curr >= '0' && curr <= '6')) {
+                dp[i] += dp[i - 2];
+            } else if (prev == '1' && (curr >= '7')) {
+                dp[i] += dp[i - 2];
+            }
+            prev = curr;
+        }
+        return dp[n];
+    }
+
+    // 
+
 }
 
 

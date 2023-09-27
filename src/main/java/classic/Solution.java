df526c7144a014ff12fa9aa6bd89fc5d1ce2f56e
benbi-hao <fahaing@163.com>
2023-09-27 22:07:03 +0800

    update all

--- src/main/java/classic/Solution.java
+++ src/main/java/classic/Solution.java
@@ -1,6 +1,7 @@
 package classic;
 
 public class Solution {
+    // 字符串匹配kmp算法
     public boolean kmp(String s, String t) {
         int[] next = buildNext(t);
         int n = s.length();
@@ -34,4 +35,26 @@ public class Solution {
         }
         return next;
     }
+
+    // 计算组合数的两种做法，推荐用乘法递推式
+    public long combinationAdd(int n, int m) {
+        long[][] dp = new long[n + 1][m + 1];
+        for (int i = 0; i <= n; i++) {
+            dp[i][0] = 1;
+            for (int j = 1; j <= m && j <= i; j++) {
+                dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j];
+            }
+        }
+        return dp[n][m];
+    }
+
+    public long combinationProduct(int n, int m) {
+        long ret = 1;
+        for (int i = m + 1; i <= n; i++) {
+            ret = ret * i / (i - m);
+        }
+        return ret;
+    }
+
+    
 }
---
+++ src/main/java/realexam/aliyun/Main.java
@@ -0,0 +1,82 @@
+package realexam.aliyun;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+
+    // 给一棵树，每个节点有正整数权值，计算路径权值乘积为6的路径的数量
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        weights = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            weights[i] = in.nextInt();
+        }
+
+        graph = new List[n + 1];
+        for (int i = 1; i <= n; i++) {
+            graph[i] = new ArrayList<>();
+        }
+
+        for (int i = 0; i < n - 1; i++) {
+            int u = in.nextInt();
+            int v = in.nextInt();
+            graph[u].add(v);
+            graph[v].add(u);
+        }
+
+        ret = 0;
+        pathCount = new int[n + 1][7];
+        dfs(0, 1);
+        System.out.println(ret);
+    }
+
+    private static int ret;
+    private static List<Integer>[] graph;
+    private static int[] weights;
+    private static int[][] pathCount;
+
+    private static int[] dfs(int parent, int curr) {
+        int[] cntToParent = pathCount[parent];
+        int[] cntToCurr = pathCount[curr];
+        int w = weights[curr];
+        handle(w, cntToParent, cntToCurr);
+        ret += cntToCurr[6];
+        cntToCurr[w] += 1;
+        int[] cntBackCurr = new int[7];
+        for (int child : graph[curr]) {
+            if (child == parent) continue;
+            int[] cntBackChild = dfs(curr, child);
+            handle(w, cntBackChild, cntBackCurr);
+            handle(w, cntBackChild, cntToCurr);
+        }
+        cntBackCurr[w] += 1;
+        return cntBackCurr;
+    }
+
+    private static void handle(int w, int[] from, int[] curr) {
+        switch(w) {
+            case 1:
+                curr[1] += from[1];
+                curr[2] += from[2];
+                curr[3] += from[3];
+                curr[6] += from[6];
+                break;
+            case 2:
+                curr[2] += from[1];
+                curr[6] += from[3];
+                break;
+            case 3:
+                curr[3] += from[1];
+                curr[6] += from[2];
+                break;
+            case 6:
+                curr[6] += from[1];
+                break;
+            default:
+                break;
+        }
+    }
+}

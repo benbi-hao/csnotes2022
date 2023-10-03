6c2aed0be57acf96d5b2a1794ce044485b9b0759
benbi-hao <fahaing@163.com>
2023-10-03 16:37:08 +0800

    update all

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,7 +3,7 @@ import leetcode.Solution;
 
 public class Main {
     public static void main(String[] args) {
-        new Solution().averageOfLevels(null);
+        System.out.println(0.3 - 0.1);
         
     }
 
